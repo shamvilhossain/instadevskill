46302d9c9f1a4c7f6204af1c937aa1acd2dcc6c0
Shamvil Hossain <shamvil.hossain@cg-bd.com>
2021-01-13 20:01:40 +0600

    migration files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_of_birth')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('account_type');
             $table->rememberToken();
             $table->timestamps();
         });
